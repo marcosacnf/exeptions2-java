7e7d0062a7556ec430c8604161b1494c2eb6fb63
Marcos Carvalho <marcosacnf12@gmail.com>
2020-11-15 16:59:56 -0300

    Exception exemple

---
+++ src/model/entities/Account.java
@@ -0,0 +1,66 @@
+package model.entities;
+
+import model.exceptions.DomainExceptions;
+
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+	}
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) throws DomainExceptions {
+		
+		if (amount > withdrawLimit) {
+			throw new DomainExceptions("The amount exceeds withdraw limit");
+		}
+		if (amount > balance) {
+			throw new DomainExceptions("Not enough balance");
+		}
+		
+		balance -= amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "New balance: " + String.format("%.2f", balance);
+	}
+}
